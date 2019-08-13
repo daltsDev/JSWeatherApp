f4919a83033181d6eafb9f9cb8fb046849d2c08b
daltsDev <daltsdev@Stephens-MacBook-Pro.local>
2019-08-13 21:37:25 +0100

    Finished the JS code and added celcius, Time to DELOPY

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ window.addEventListener('load', () => {
     let temperatureDegree = document.querySelector('.temperature-degree');
     let temperatureDescription = document.querySelector('.temperature-description');
     let locationTimezone = document.querySelector('.location-timzone');
-
+    let temperatureSection = document.querySelector('.temperature');
+    let temperatureSpan = document.querySelector('.temperature span')
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(postition => {
@@ -18,14 +19,34 @@ window.addEventListener('load', () => {
             })
             .then(data => {
                 console.log(data);
-                const {temperature, summary} = data.currently;
+                const {temperature, summary, icon} = data.currently;
                 // set dom elements from api
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
-            })
+                //celcius conversion
+                let celcius = (temperature - 32) * (5/9);
+                setIcons(icon, document.querySelector('.icon'));
+
+                //adding change to celcius funct 
+                temperatureSection.addEventListener('click', ()=> {
+                    if(temperatureSpan.textContent === 'F'){
+                        temperatureSpan.textContent = 'C';
+                        temperatureDegree.textContent = Math.floor(celcius);
+                    } else {
+                        temperatureSpan.textContent = 'F';
+                        temperatureDegree.textContent = temperature;
+                    }
+                })
+            });
         });
     }else {
         alert("Your browser does not support this");
+    }
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({ color:"white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
